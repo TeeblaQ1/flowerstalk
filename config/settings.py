7cb4765fae71720bdc8459fe8aa7845efc453358
Oluwatosin Olasupo <oluwateeblaq@gmail.com>
2022-01-12 01:05:30 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['flowerstalkng.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
