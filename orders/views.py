fed76aeafaa20053521c1ce2d0a86b6a3229994e
Oluwatosin Olasupo <oluwateeblaq@gmail.com>
2022-01-24 20:54:16 +0100

    check for error

--- orders/views.py
+++ orders/views.py
@@ -8,6 +8,7 @@ from cart.cart import Cart
 # Create your views here.
 def order_create(request):
     cart = Cart(request)
+    vat = 0
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
