8f3c3317dbb826c4a4e151cf5fa0035a68bfdab1
Oluwatosin Olasupo <oluwateeblaq@gmail.com>
2022-02-03 10:20:40 +0100

    Changed admin site header and title

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Flowerstalk Administration'
+admin.site.site_title = 'Flowerstalk admin site'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
